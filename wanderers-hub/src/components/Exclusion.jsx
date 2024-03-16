3f8bc768db31bf62122e70e74d6809bfe7156bb6
raincoat4 <lr1tmrr@gmail.com>
2024-03-16 16:21:10 -0700

    removed country selector added camel

---
+++ wanderers-hub/src/components/Exclusion.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from './Button'
+import Image from 'next/image';
+
+const Exclusion = () => {
+  return (
+    <div className="bg-[url('/blurrybg.png')] bg-cover bg-center bg-no-repeat h-screen">
+        <div className="w-[512px] h-[470px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-25 rounded-2xl border border-white border-opacity-20">
+            <div className = "w-96 text-white text-4xl m-12">
+                Help us Optimize Your Experience
+            </div>
+            <div className = "w-96 text-white text-xl m-9">
+                Are there any destinations you would like us to exclude from your results? Select all that apply, then press ‘Next’
+                {/* button goes here */}
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Exclusion
--- wanderers-hub/src/components/Homepagebg.jsx
+++ wanderers-hub/src/components/Homepagebg.jsx
@@ -5,8 +5,10 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const Homepagebg = () => {
     const scene = new THREE.Scene();
-    const modelRef = useRef(null);
-    const modelLoaded = useRef(false);
+    const modelRaccoon = useRef(null);
+    const modelLoadedRaccoon = useRef(false);
+    const modelCamel = useRef(null);
+    const modelLoadedCamel = useRef(false);
     const axis = new THREE.Vector3(1,5,1).normalize();
     var left = true;
       useEffect(() => {
@@ -23,19 +25,30 @@ const Homepagebg = () => {
           camera.position.set(4 * vw, 3 * vh, 8 * vh); // // Adjust coefficients as needed
   
           const loader = new GLTFLoader();
-         // const controls = new OrbitControls(camera, renderer.domElement);
+          const controls = new OrbitControls(camera, renderer.domElement);
+         //raccoon
           loader.load('/low_poly_raccoon.glb', gltf => {
-            if (!modelLoaded.current) {
+            if (!modelLoadedRaccoon.current) {
               const raccoon = gltf.scene;
               raccoon.scale.set(5, 5, 5);
               raccoon.position.set(0, 0, 0);
-              raccoon.rotateOnAxis(axis, -2);
               scene.add(raccoon);
-              modelRef.current = raccoon;
-              modelLoaded.current = true;
+              modelRaccoon.current = raccoon;
+              modelLoadedRaccoon.current = true;
             }
           });
-  
+          
+          //camel
+          loader.load('/low_poly_camel.glb', gltf => {
+            if (!modelLoadedCamel.current) {
+              const camel = gltf.scene;
+              camel.scale.set(0.5, 0.5, 0.5);
+              camel.position.set(5, 0, 0);
+              scene.add(camel);
+              modelCamel.current = camel;
+              modelLoadedCamel.current = true;
+            }
+          });
           const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
           scene.add(ambientLight);
           const pointLight = new THREE.PointLight(0xffffff, 1000);
@@ -50,19 +63,19 @@ const Homepagebg = () => {
           scene.background = bg;
           function animate(){
               requestAnimationFrame(animate);
-            //  controls.update()
-             if (modelRef.current) {
-              if(left < 200){
-                modelRef.current.rotateOnAxis(axis, 0.005);
-                left += 1;
-              }
-              else if(left < 400){
-                modelRef.current.rotateOnAxis(axis, -0.005);
-                left += 1;
-              }
-              else{
-                left = 0;
-              }
+              controls.update()
+             if (modelRaccoon.current) {
+              // if(left < 200){
+              //   modelRaccoon.current.rotateOnAxis(axis, 0.005);
+              //   left += 1;
+              // }
+              // else if(left < 400){
+              //   modelRaccoon.current.rotateOnAxis(axis, -0.005);
+              //   left += 1;
+              // }
+              // else{
+              //   left = 0;
+              // }
              }
   
               renderer.render(scene, camera);
