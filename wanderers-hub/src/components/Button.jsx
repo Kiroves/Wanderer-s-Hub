643af79891fe5927c1a0b07e531e2103b5ecda76
raincoat4 <lr1tmrr@gmail.com>
2024-03-16 14:16:18 -0700

    added homepage button

--- wanderers-hub/src/components/Button.jsx
+++ wanderers-hub/src/components/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useRouter } from 'next/navigation';
 
-const Button = () => {
+const Button = (props) => {
     const router = useRouter();
     return (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <button onClick={() => router.push('/picklocation')} className="bg-green-500 text-white px-4 py-2 rounded-full">go</button>
-        </div>
+        <button onClick = {props.function} className="absolute top-96 left-48 w-64 h-16 px-7 py-3 bg-gradient-to-b from-purple-500 to-blue-400 rounded-xl shadow-inner border border-blue-700 border-opacity-30 backdrop-blur-lg justify-center items-center gap-3 inline-flex">
+            <div className="text-center text-white text-xl font-medium font-['Inter'] leading-tight">{props.text}</div>
+        </button>
     )
 }
 
--- wanderers-hub/src/components/RaccoonLogin.jsx
+++ wanderers-hub/src/components/RaccoonLogin.jsx
@@ -1,82 +1,101 @@
 import * as THREE from 'three';
 import React, { useEffect, useRef } from "react";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const RaccoonLogin = () => {
   const scene = new THREE.Scene();
   const modelRef = useRef(null);
   const modelLoaded = useRef(false);
-  const axis = new THREE.Vector3(1,7,1).normalize();
-  const axis2 = new THREE.Vector3(-4,-8,-7)
+  const axis = new THREE.Vector3(1, 5, 1).normalize();
+  const camera = useRef(null);
+  const renderer = useRef(null);
   var left = true;
-    useEffect(() => {
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer({
-        canvas: document.querySelector('#login-bg'),
-        });
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        const vw = window.innerWidth * 0.01;
-        const vh = window.innerHeight * 0.01;
-        camera.position.set(4 * vw, 3 * vh, -3 * vh); // Adjust coefficients as needed
-        camera.lookAt(axis2);
-        const loader = new GLTFLoader();
-       // const controls = new OrbitControls(camera, renderer.domElement);
-        loader.load('/low_poly_raccoon.glb', gltf => {
-          if (!modelLoaded.current) {
-            const raccoon = gltf.scene;
-            raccoon.scale.set(5, 5, 5);
-            raccoon.position.set(0, 0, 0);
-            //raccoon.rotateOnAxis(axis, 0);
-            scene.add(raccoon);
-            modelRef.current = raccoon;
-            modelLoaded.current = true;
-          }
-        });
 
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
-        scene.add(ambientLight);
-        const pointLight = new THREE.PointLight(0xffffff, 1000);
-        //const lightHelper = new THREE.PointLightHelper(pointLight);
-        //const gridHelper = new THREE.GridHelper(200, 50);
-        scene.add(pointLight);
-        pointLight.position.set(-3 * vw, 3 * vh, -4 * vh);
+  useEffect(() => {
+    const vw = window.innerWidth * 0.01;
+    const vh = window.innerHeight * 0.01;
 
-       // const axesHelper = new THREE.AxesHelper(10); // Length of each axis
-        //scene.add(axesHelper);
-        const bg = new THREE.TextureLoader().load('/racbg.png');
-        scene.background = bg;
-        function animate(){
-            requestAnimationFrame(animate);
-          //  controls.update()
-           if (modelRef.current) {
-            if(left < 200){
-              modelRef.current.rotateOnAxis(axis, 0.005);
-              left += 1;
-            }
-            else if(left < 400){
-              modelRef.current.rotateOnAxis(axis, -0.005);
-              left += 1;
-            }
-            else{
-              left = 0;
-            }
-           }
+    // Create camera
+    camera.current = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.current.rotation.x = -Math.PI / vw * 1.5;
+    camera.current.rotation.z = 0;
+    camera.current.position.set(4 * vw, 3 * vh, 8 * vh); // Adjust coefficients as needed
 
-            renderer.render(scene, camera);
+    // Create renderer
+    renderer.current = new THREE.WebGLRenderer({
+      canvas: document.querySelector('#login-bg'),
+    });
+    renderer.current.setPixelRatio(window.devicePixelRatio);
+    renderer.current.setSize(window.innerWidth, window.innerHeight);
+
+    const loader = new GLTFLoader();
+    loader.load('/low_poly_raccoon.glb', gltf => {
+      if (!modelLoaded.current) {
+        const raccoon = gltf.scene;
+        raccoon.scale.set(5, 5, 5);
+        raccoon.rotateOnAxis(axis, -2);
+        scene.add(raccoon);
+        modelRef.current = raccoon;
+        modelLoaded.current = true;
+
+        // Adjust camera position based on raccoon's position
+        const boundingBox = new THREE.Box3().setFromObject(raccoon);
+        const center = boundingBox.getCenter(new THREE.Vector3());
+        const size = boundingBox.getSize(new THREE.Vector3());
+        const maxDim = Math.max(size.x, size.y, size.z);
+        const fov = camera.current.fov * (Math.PI / 180);
+        let cameraZ = Math.abs((maxDim / 4) * Math.tan(fov * 2));
+        camera.current.position.set(center.x + 10, center.y, center.z + cameraZ * 2); // Adjust z-coordinate here
+      }
+    });
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
+    scene.add(ambientLight);
+    const pointLight = new THREE.PointLight(0xffffff, 1000);
+    scene.add(pointLight);
+    pointLight.position.set(-3 * vw, 3 * vh, -4 * vh);
+
+    const bg = new THREE.TextureLoader().load('/racbg.png');
+    scene.background = bg;
+
+    function handleResize() {
+      const vw = window.innerWidth * 0.01;
+      const vh = window.innerHeight * 0.01;
+      camera.current.aspect = window.innerWidth / window.innerHeight;
+      camera.current.updateProjectionMatrix();
+      renderer.current.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    function animate() {
+      requestAnimationFrame(animate);
+      if (modelRef.current) {
+        if (left < 200) {
+          modelRef.current.rotateOnAxis(axis, 0.005);
+          left += 1;
         }
-        animate();
+        else if (left < 400) {
+          modelRef.current.rotateOnAxis(axis, -0.005);
+          left += 1;
+        }
+        else {
+          left = 0;
+        }
+      }
+      renderer.current.render(scene, camera.current);
+    }
+    animate();
 
-        return () => {
-        };
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
 
-    }, []);
-
+  }, []);
 
   return (
     <canvas id="login-bg" className="absolute inset-0 w-full h-full"></canvas>
   )
 }
 
-export default RaccoonLogin
+export default RaccoonLogin;
