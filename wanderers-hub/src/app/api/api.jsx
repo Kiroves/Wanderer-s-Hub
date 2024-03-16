5106378b971741ac3412d2e4d848a16fddb408c3
Chocomelk <gabe.o7578@gmail.com>
2024-03-15 22:01:28 -0700

    google api access +tests

---
+++ wanderers-hub/src/app/api/api.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const GoogleMapsComponent = () => {
+  const googleMapsAPIkey =  process.env.NEXT_PUBLIC_REACT_APP_MAPS_API_KEY;
+
+  const searchPlace = async (q) => {
+    const searchTextURL = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${googleMapsAPIkey}&query=${q}`;
+
+    try {
+      const response = await axios.get(searchTextURL);
+      const placeId = response.data.results[0].place_id;
+
+      const placeDetailsURL = `https://maps.googleapis.com/maps/api/place/details/json?fields=photos&place_id=${placeId}&key=${googleMapsAPIkey}`;
+        
+      try {
+        const detailsResponse = await axios.get(placeDetailsURL);
+        const references = detailsResponse.data.result.photos.map(photo => photo.photo_reference).slice(0, 5);
+        photoReferences= (references);
+      } catch (detailsError) {
+        console.log('Error fetching place details:', detailsError);
+      }
+    } catch (error) {
+      console.log('Error fetching place:', error);
+    }
+  };
+    return (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <button onClick={searchPlace("vancouver")} className="bg-green-500 text-white px-4 py-2 rounded-full">go</button>
+        </div>
+    );
+};
+
+export default GoogleMapsComponent;
--- wanderers-hub/src/app/page.js
+++ wanderers-hub/src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import Button from '@/components/Button';
+import GoogleMapsComponent from '../app/api/api.jsx';
 import Donut from '@/components/Donut';
 import Login from '@/components/Login';
 import { useRouter } from 'next/navigation';
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <div className="relative h-screen">
       <Donut />
-      <Button />
+      <GoogleMapsComponent />
       <Login/>
       <div className="absolute top-0 left-0">
             <button onClick={() => console.log("hi")} className="bg-green-500 text-white px-4 py-2 rounded-full">test</button>
