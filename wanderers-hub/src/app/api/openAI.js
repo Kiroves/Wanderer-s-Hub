78487827fb5a4ebe6ccb31223ccdaf69d6352732
Chocomelk <gabe.o7578@gmail.com>
2024-03-16 19:34:44 -0700

    Adjust ai response interfaced with exclusion page

--- wanderers-hub/src/app/api/openAI.js
+++ wanderers-hub/src/app/api/openAI.js
@@ -2,15 +2,14 @@ import { CohereClient } from "cohere-ai";
 
 
 
-
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 export default class countryWanderers {
     countryWanderersMembers =[
         {
             name:"Camel",
-            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries where there's a lot of unique cultures, and your response should be no more than 50 words.",
-            no:"Do not include these countries.",
-            type:"laidback and hippie like",
-            conversation:[],
+            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries that have warmer climates. Answer as if answering this question: Where should I go for vacation? Try to generate a country randomized based on the second of day. Answer with this for mat (COUNTRY NAME)! (reason..)",
         },/*
         {
             name:"PolarBear",
@@ -49,26 +48,25 @@ export default class countryWanderers {
 
 
     async queryWanderers(noAllowed) {
+        var res='';
         try {
-            this.membersJson.query.push(this.membersJson.members[0].settings+"Do not include these countries: "+noAllowed+". State the country name as your first word");
+            this.membersJson.query.push(this.membersJson.members[0].settings+"DO NOT CONSIDER THESE COUNTRIES: "+noAllowed.toString());
 
             const cohere= new CohereClient({
                 token: process.env.NEXT_PUBLIC_REACT_APP_COHERE_API_KEY,
             });
 
-            
-                const res = await cohere.generate({
+                res = await cohere.generate({
                     prompt:this.membersJson.query[0],
                     maxTokens:150,
                 });
-
-                console.log(res);
+                
             }catch(e){
                 console.log(e);
             }
             
             console.log(this.membersJson);
-            console.log("thumb");
+            console.log(res);
             return "thumb";
         } catch (e) {
             console.log(e);
