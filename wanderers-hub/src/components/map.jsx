8a64ab9e3cb67f74891004504a5602613804e382
Chocomelk <gabe.o7578@gmail.com>
2024-03-17 05:54:20 -0700

    search back in components

--- wanderers-hub/src/components/map.jsx
+++ wanderers-hub/src/components/map.jsx
@@ -5,49 +5,50 @@ import { int } from 'three/examples/jsm/nodes/shadernode/ShaderNode';
 var map;
 var placeId='';
 var references=[];
-export const searchPlace =async (q  , lat, lng) => {
-  var center = {lat:lat, lng:lng};
-  map = new google.maps.Map(
-    document.getElementById('map'),{center,zoom:7}
-  )
-  var request={
-    query:q,
-    fields:['place_id'],
-  };
 
-
-  var service = new google.maps.places.PlacesService(map);
-
-  await service.findPlaceFromQuery(request,function(results,status){
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      console.log(results);
-      placeId=results[0].place_id;
-    }
-  });
-  console.log(placeId);
-  request={
-    placeId:placeId,
-    fields:['photos']
-  };
-  const opts={
-    maxHeight:int,
-    maxWidth:int,
-  };
-  await service.getDetails(request,function(results,status){
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      console.log(results.photos[1].getUrl());
-      for(let i =0; i<5; i++){
-        references.push(results.photos[i].getUrl([opts]));
-      }
-    }
-  });
-  return references;
-};
 
 const GoogleMapsComponent = () => {
   const googleMapsAPIkey = process.env.NEXT_PUBLIC_REACT_APP_MAPS_API_KEY;
   //center needs to be google.maps.LatLng(lat,lng);
-
+  const searchPlace =async (q  , lat, lng) => {
+  
+    var center = {lat:lat, lng:lng};
+    map = new google.maps.Map(
+      document.getElementById('map'),{center,zoom:7}
+    )
+    var request={
+      query:q,
+      fields:['place_id'],
+    };
+  
+  
+    var service = new google.maps.places.PlacesService(map);
+  
+    await service.findPlaceFromQuery(request,function(results,status){
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
+        placeId=results[0].place_id;
+      }
+    });
+    console.log(placeId);
+    request={
+      placeId:placeId,
+      fields:['photos']
+    };
+    const opts={
+      maxHeight:int,
+      maxWidth:int,
+    };
+    await service.getDetails(request,function(results,status){
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results.photos[1].getUrl());
+        for(let i =0; i<5; i++){
+          references.push(results.photos[i].getUrl([opts]));
+        }
+      }
+    });
+    return references;
+  };
   return (
     <LoadScript
       googleMapsApiKey={googleMapsAPIkey}
