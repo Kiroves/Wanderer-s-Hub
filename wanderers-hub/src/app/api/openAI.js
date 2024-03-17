a87739636c4bba035cecd3b4c5382300e2d71119
Chocomelk <gabe.o7578@gmail.com>
2024-03-16 22:56:57 -0700

    ai return value updated& formatted

--- wanderers-hub/src/app/api/openAI.js
+++ wanderers-hub/src/app/api/openAI.js
@@ -7,23 +7,23 @@ export default class countryWanderers {
     countryWanderersMembers =[
         {
             name:"Camel",
-            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries that have warmer climates. Answer as if answering this question: Where should I go for vacation? Try to generate a country randomized based on the milisecond of day. Answer with this for mat (COUNTRY NAME)! (reason..)",
+            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries that have warmer climates. Answer as if answering this question: Where should I go for vacation? Try to generate a country randomized based on the milisecond of day. Answer with this format (COUNTRY NAME)! (reason..)",
         },
         {
             name:"PolarBear",
-            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries that have colder climates. Answer as if answering this question: Where should I go for vacation? Try to generate a country randomized based on the milisecond of day. Answer with this for mat (COUNTRY NAME)! (reason..)",
+            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries that have colder climates. Answer as if answering this question: Where should I go for vacation? Try to generate a country randomized based on the milisecond of day. Answer with this format (COUNTRY NAME)! (reason..)",
             type:"a history buff",
             conversation:[],
         },
         {
             name:"Monkey",
-            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries that have a lot of events/food/entertainment. Answer as if answering this question: Where should I go for vacation? Try to generate a country randomized based on the milisecond of day. Answer with this for mat (COUNTRY NAME)! (reason..)",
+            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries that have a lot of events/food/entertainment. Answer as if answering this question: Where should I go for vacation? Try to generate a country randomized based on the milisecond of day. Answer with this format (COUNTRY NAME)! (reason..)",
             type:"energetic party animal",
             conversation:[],
         },
         {
             name:"Racoon",
-            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries that have lots of adreneline filled adventures. Answer as if answering this question: Where should I go for vacation? Try to generate a country randomized based on the milisecond of day. Answer with this for mat (COUNTRY NAME)! (reason..)",
+            settings:"You're on a board, giving your opinion on what country the traveller should go for their vacation. You prefer countries that have lots of adreneline filled adventures. Answer as if answering this question: Where should I go for vacation? Try to generate a country randomized based on the milisecond of day. Answer with this format (COUNTRY NAME)! (reason..)",
             type:"adreneline junky",
             conversation:[],
         },
@@ -44,24 +44,22 @@ export default class countryWanderers {
 
     async queryWanderers(noAllowed) {
         var res='';
-        var no='';
+        var ans=[];
         try {
             const cohere= new CohereClient({
                 token: process.env.NEXT_PUBLIC_REACT_APP_COHERE_API_KEY,
             });
             
             const no = noAllowed.map(obj=>obj.value);
-            console.log(no);
             for(let i=0;i<4;i++){
-                
                 this.membersJson.query.push(this.membersJson.members[i].settings+"DO NOT CONSIDER THESE COUNTRIES: "+ no.toString());
                 console.log(this.membersJson.query[i]);
                 res = await cohere.generate({
-                    
                     prompt:this.membersJson.query[i],
                     maxTokens:150,
                 });
-                
+                ans.push(res.generations[0].text);
+                console.log(ans);
                 
             }
             
@@ -72,7 +70,7 @@ export default class countryWanderers {
             
             console.log(this.membersJson);
             
-            return "thumb";
+            return ans;
         } catch (e) {
             console.log(e);
             return "Error";
